de419bfe0ad330c3ddf54334e094350516239356
saugattimsina <saugattimsina97@gmail.com>
2021-03-10 11:02:25 +0545

    database created

---
+++ database_connection.py
@@ -0,0 +1,53 @@
+import psycopg2
+
+def database_creator():
+    try:
+        conn = psycopg2.connect(database="raidforum",user="saugat1",password="saugat123",host = "127.0.0.1",port = "5432")
+        cur = conn.cursor()
+        print(cur)
+
+        table_schema1 = '''CREATE TABLE IF NOT EXISTS  posts(
+            post_id INT GENERATED ALWAYS AS IDENTITY,
+            page_url VARCHAR(255) UNIQUE,
+            page_title VARCHAR(255),
+            text_content TEXT,
+            date_time  VARCHAR(50),
+            username  TEXT,
+            user_title VARCHAR(50),
+            PRIMARY KEY(post_id)
+        )
+        '''
+        table_schema2 = ''' 
+        CREATE TABLE IF NOT EXISTS comments(
+            comment_id INT GENERATED ALWAYS AS IDENTITY,
+            text_content TEXT,
+            date_time  VARCHAR(50),
+            username  TEXT,
+            user_title VARCHAR(50),
+            post_id  INT,
+            PRIMARY KEY(comment_id),
+            CONSTRAINT fk_posts FOREIGN KEY(post_id) REFERENCES posts(post_id)
+        )   
+        '''
+        cur.execute(table_schema1)
+
+        cur.execute(table_schema2)
+        conn.commit()
+        conn.close()
+        print("success")
+    except Exception as e:
+        print(e)
+
+  
+
+def connection_creator():
+
+    try:  
+        conn = psycopg2.connect(database="raidforum",user="saugat1",password="saugat123",host = "127.0.0.1",port = "5432")
+        
+        return conn
+    except Exception as e:
+        print(e)
+    
+    
+database_creator()
